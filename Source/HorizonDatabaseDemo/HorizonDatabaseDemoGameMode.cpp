ce1e2dc98343e67f6a7e0494404651e0dacafe24
dorgon <dorgonman@hotmail.com>
2016-10-04 17:50:26 +0800

    update

--- Source/HorizonDatabaseDemo/HorizonDatabaseDemoGameMode.cpp
+++ Source/HorizonDatabaseDemo/HorizonDatabaseDemoGameMode.cpp
@@ -3,7 +3,7 @@
 #include "HorizonDatabaseDemo.h"
 #include "HorizonDatabaseDemoGameMode.h"
 
-#include "Horizon/Database/HorizonDatabase.h"
+//#include "Horizon/Database/HorizonDatabase.h"
 
 
 #include <string>
@@ -52,58 +52,58 @@ AHorizonDatabaseDemoGameMode::AHorizonDatabaseDemoGameMode() {
 
 
 	//../../../HorizonDatabaseDemo/PersistentDownloadDir/Horizon/
-
-	FString myFolder = "/Horizon/";
-#if (PLATFORM_ANDROID)
-	extern FString GExternalFilePath;
-	FString folderPath = GExternalFilePath + myFolder;//androidPlatformFile.FileRootPath(TEXT("")) + FPaths::GamePersistentDownloadDir() 
-#else
-	FString folderPath = FPaths::GamePersistentDownloadDir() + myFolder;
-	//folderPath = FPaths::ConvertRelativePathToFull(folderPath);
-#endif
-
-	CreateDirectoryRecursively(folderPath);
-
-
-	FString connectString = folderPath + FString("save.db");
-
-
-	UE_LOG(LogTemp, Log, TEXT("======================connectString: %s"), *connectString);
-	//const soci::backend_factory* backEnd = soci::factory_sqlite3();
-
-
-	//const soci::backend_factory* backEnd = soci::factory_sqlite3();
-	//soci::session sql(*backEnd, "/sdcard/UE4Game/HorizonDatabaseDemo/HorizonDatabaseDemo/PersistentDownloadDir/Horizon/myFullPath.db");  //android can't open db if path is relative
-	//soci::session sql(*backEnd, std::string(TCHAR_TO_UTF8(*connectString)));
-	soci::session* sql = new soci::session(soci::sqlite3, std::string(TCHAR_TO_UTF8(*connectString)));
-	//HorizonDatabase db;
-	try {
-		*sql << "drop table if exists test1";
-		*sql <<
-			"create table test1 ("
-			"    id integer,"
-			"    name varchar(100)"
-			")";
-		*sql << "insert into test1(id, name) values(7, \'John\')";
-
-		//sql << "insert into test1(id, name) ddd values(7, \'John\')";
-	}
-	catch (...) {
-		//UE_HORIZON_LOG("oops! something error");
-	}
-	soci::rowid rid(*sql);
-	*sql << "select oid from test1 where id = 7", soci::into(rid);
-
-	int id;
-	std::string name;
-
-	*sql << "select id, name from test1 where oid = :rid",
-		soci::into(id), soci::into(name), soci::use(rid);
-
-	UE_LOG(LogTemp, Log, TEXT("=============TEST select from sqlite: %d, %s"), id, *FString(name.c_str()));
-
-	*sql << "drop table test1";
-
+//
+//	FString myFolder = "/Horizon/";
+//#if (PLATFORM_ANDROID)
+//	extern FString GExternalFilePath;
+//	FString folderPath = GExternalFilePath + myFolder;//androidPlatformFile.FileRootPath(TEXT("")) + FPaths::GamePersistentDownloadDir() 
+//#else
+//	FString folderPath = FPaths::GamePersistentDownloadDir() + myFolder;
+//	//folderPath = FPaths::ConvertRelativePathToFull(folderPath);
+//#endif
+//
+//	CreateDirectoryRecursively(folderPath);
+//
+//
+//	FString connectString = folderPath + FString("save.db");
+//
+//
+//	UE_LOG(LogTemp, Log, TEXT("======================connectString: %s"), *connectString);
+//	//const soci::backend_factory* backEnd = soci::factory_sqlite3();
+//
+//
+//	//const soci::backend_factory* backEnd = soci::factory_sqlite3();
+//	//soci::session sql(*backEnd, "/sdcard/UE4Game/HorizonDatabaseDemo/HorizonDatabaseDemo/PersistentDownloadDir/Horizon/myFullPath.db");  //android can't open db if path is relative
+//	//soci::session sql(*backEnd, std::string(TCHAR_TO_UTF8(*connectString)));
+//	soci::session* sql = new soci::session(soci::sqlite3, std::string(TCHAR_TO_UTF8(*connectString)));
+//	//HorizonDatabase db;
+//	try {
+//		*sql << "drop table if exists test1";
+//		*sql <<
+//			"create table test1 ("
+//			"    id integer,"
+//			"    name varchar(100)"
+//			")";
+//		*sql << "insert into test1(id, name) values(7, \'John\')";
+//
+//		//sql << "insert into test1(id, name) ddd values(7, \'John\')";
+//	}
+//	catch (...) {
+//		//UE_HORIZON_LOG("oops! something error");
+//	}
+//	soci::rowid rid(*sql);
+//	*sql << "select oid from test1 where id = 7", soci::into(rid);
+//
+//	int id;
+//	std::string name;
+//
+//	*sql << "select id, name from test1 where oid = :rid",
+//		soci::into(id), soci::into(name), soci::use(rid);
+//
+//	UE_LOG(LogTemp, Log, TEXT("=============TEST select from sqlite: %d, %s"), id, *FString(name.c_str()));
+//
+//	*sql << "drop table test1";
+//
 
 	/*sqlite3 *db;
 	char *zErrMsg = 0;
